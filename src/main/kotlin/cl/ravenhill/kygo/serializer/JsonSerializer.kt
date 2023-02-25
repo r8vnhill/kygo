2cb6d8c7be13457b06225fce11b0f561ee2eec28
R8V <ignacio.slater@ug.uchile.cl>
2023-02-24 22:40:54 -0300

    FEAT Json and YAML serializers

---
+++ src/main/kotlin/cl/ravenhill/kygo/serializer/JsonSerializer.kt
@@ -0,0 +1,19 @@
+package cl.ravenhill.kygo.serializer
+
+import cl.ravenhill.kygo.cards.Card
+
+/**
+ * Class for JSON card serializers.
+ *
+ * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ */
+class JsonSerializer : AbstractCardSerializer() {
+  override fun serialize(card: Card): String {
+    return """
+      |{
+      | "name": "${card.name}",
+      | "text": "${card.text}"
+      |}
+      """.trimMargin()
+  }
+}
---
+++ src/main/kotlin/cl/ravenhill/kygo/serializer/YamlSerializer.kt
@@ -0,0 +1,18 @@
+package cl.ravenhill.kygo.serializer
+
+import cl.ravenhill.kygo.cards.Card
+
+/**
+ * Class for YAML card serializers.
+ *
+ * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ */
+class YamlSerializer : AbstractCardSerializer() {
+  override fun serialize(card: Card): String {
+    return """
+        |!!Card
+        |name: ${card.name}
+        |text: ${card.text}
+        """.trimMargin()
+  }
+}
